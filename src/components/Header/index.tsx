c7bcd3489760e8189b47f10b0ad22a604482303f
Chrystian Rodolfo <chrystianrz@hotmail.com>
2024-11-06 15:33:31 -0300

    feat: :sparkles: add HomePage and base Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className="container">
+        <h1 className="title one">Hardware E-Commerce</h1>
+      </div>
+    </header>
+  );
+};
